8653b24a8a5044a02df16ad4679038cc52d2a97e
antonkulaga <antonkulaga@gmail.com>
2014-10-02 04:36:45 +0300

    fix of names for windows pcs

---
+++ threejs/src/main/scala/org/scalajs/threejs/extensions/HoverControls.scala
@@ -0,0 +1,10 @@
+package org.scalajs.threejs.extensions
+
+import org.scalajs.threejs.Camera
+
+
+class HoverControls(camera:Camera) {
+
+  //TODO:write it. It should do the same as OrbitControls
+
+}
